050a765494a827dabd4c0cdfe81585a4aee2e32f
Akashyadav1402 <162121569+Akashyadav1402@users.noreply.github.com>
2025-04-21 19:35:46 +0530

    Fix: flake8 linting issues

--- test_app.py
+++ test_app.py
@@ -1,7 +1,9 @@
 from app import add, subtract
 
+
 def test_add():
-    assert add(3, 4) == 7
+    assert add(2, 3) == 5
+
 
 def test_subtract():
-    assert subtract(5, 2) == 3
+    assert subtract(5, 3) == 2
